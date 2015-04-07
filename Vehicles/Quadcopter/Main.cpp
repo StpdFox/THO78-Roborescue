ffc0fbf90b13aa37863b0fc536263207477c42c2
KjeldPerquin <kjeld.perquin@student.hu.nl>
2015-04-07 11:43:30 +0200

    Quadcopter threading example

--- Vehicles/Quadcopter/Main.cpp
+++ Vehicles/Quadcopter/Main.cpp
@@ -3,14 +3,19 @@
 #include <string>
 #include "MAVLinkExchanger.h"
 #include <iostream>
+#include <thread>
 
 int main() 
 {
 	SerialPort serialPort("COM4");
 	MAVLinkExchanger exchanger{ serialPort };
 	Quadcopter quadcopter(exchanger);
+	std::thread exchangerThread{ &MAVLinkExchanger::loop, &exchanger };
+	std::thread quadcopterThread{ &Quadcopter::loop, &quadcopter };
+	exchangerThread.detach();
+	quadcopterThread.detach();
 	while (1)
 	{
-
+		//Doe dingen
 	}
 }
